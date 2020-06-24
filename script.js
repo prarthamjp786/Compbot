6cfae32c58c0d21b01b5b6be2c0d076f13842d58
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-06-24 18:35:22 -0400

    Final commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const pauseButton = document.getElementById('pause-button');
 const stopButton = document.getElementById('stop-button');
 const textInput = document.getElementById('text');
 const speedInput = document.getElementById('speed');
+let currentCharacter;
 
 playButton.addEventListener('click', () => {
     playText(textInput.value);
@@ -10,12 +11,29 @@ playButton.addEventListener('click', () => {
 
 pauseButton.addEventListener('click', pauseText);
 
+stopButton.addEventListener('click', stopText);
+
+speedInput.addEventListener('input', () => {
+    stopText();
+    playText(utterance.text.substring(currentCharacter));
+});
+
+const utterance = new SpeechSynthesisUtterance(text);
+
+utterance.addEventListener('end', () => {
+    textInput.disabled = false;
+});
+utterance.addEventListener('boundary', e => {
+    currentCharacter = e.charIndex
+});
+
 function playText(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume();
+    }
+    if (speechSynthesis.speaking) return;
+    utterance.text = text;
     utterance.rate = speedInput.value || 1;
-    utterance.addEventListener('end', () => {
-        textInput.disabled = false;
-    });
     textInput.disabled = true;
     speechSynthesis.speak(utterance);
 }
@@ -24,4 +42,9 @@ function pauseText() {
     if (speechSynthesis.speaking) {
         speechSynthesis.pause();
     }
+}
+
+function stopText() {
+    speechSynthesis.resume();
+    speechSynthesis.cancel();
 }
