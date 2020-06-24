fa93bd2dd06301608cfc01b9ff6e577651e9e223
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-06-24 14:33:21 -0400

    Text to speech logic added

---
+++ script.js
@@ -0,0 +1,27 @@
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const stopButton = document.getElementById('stop-button');
+const textInput = document.getElementById('text');
+const speedInput = document.getElementById('speed');
+
+playButton.addEventListener('click', () => {
+    playText(textInput.value);
+});
+
+pauseButton.addEventListener('click', pauseText);
+
+function playText(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = speedInput.value || 1;
+    utterance.addEventListener('end', () => {
+        textInput.disabled = false;
+    });
+    textInput.disabled = true;
+    speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) {
+        speechSynthesis.pause();
+    }
+}
